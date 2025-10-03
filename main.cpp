bc68e340ec7e70c555fb046cbc67ad13f802ba46
Shiqi Lin <115582718+ShiqiLin17@users.noreply.github.com>
2025-10-03 10:38:26 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include "lab4_problem1.h"
+#include "lab4_problem2.h"
+#include <cstdlib>  // for free()
+
+int main() {
+    char testStr[] = "hello";
+
+    int length = cstrlen(testStr);
+    std::cout << "Length of \"" << testStr << "\": " << length << std::endl;
+
+    char* echoed = echo(testStr);
+    std::cout << "Echoed string: " << echoed << std::endl;
+
+    std::free(echoed);
+
+    return 0;
+}
 
